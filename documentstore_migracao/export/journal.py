7f9a63689346984a1dac079b8790ee752b5651a7
Patricia Morimoto <patricia.morimoto@scielo.org>
2019-03-26 10:16:42 -0300

    [WIP] Adiciona extração de dados do SciELO Manager

--- documentstore_migracao/export/journal.py
+++ documentstore_migracao/export/journal.py
@@ -1,5 +1,9 @@
 """ module to export journal data """
+import datetime
+import json
 import logging
+
+import sqlalchemy
 from xylose.scielodocument import Journal
 from articlemeta.client import RestfulClient
 
@@ -7,6 +11,200 @@ from documentstore_migracao import config
 from documentstore_migracao.utils import request, extract_isis
 
 logger = logging.getLogger(__name__)
+
+
+issues_tables = [
+    {
+        'table_key': 'editorialmanager_editorialboard',
+        'tables_names': [
+            {
+                'table_key': 'editorialmanager_editorialmember',
+                'tables_names': [
+                    {
+                        'table_key': 'editorialmanager_roletype',
+                        'tables_names': [
+                            {'table_key': 'editorialmanager_roletypetranslation'},
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {'table_key': 'journalmanager_issuetitle'},
+    {'table_key': 'journalmanager_regularpressrelease'},
+]
+
+
+journals_tables = [
+    {
+        'table_key': 'journalmanager_issue',
+        'tables_names': issues_tables,
+    },
+    {
+        'table_key': 'journalmanager_aheadpressrelease',
+        'tables_names': [
+            {
+                'table_key': 'journalmanager_pressrelease',
+                'tables_names': [
+                    {'table_key': 'journalmanager_pressreleasearticle'},
+                    {'table_key': 'journalmanager_pressreleasetranslation'},
+                ],
+            },
+        ],
+    },
+    {
+        'table_key': 'journalmanager_section',
+        'tables_names': [
+            {'table_key': 'journalmanager_sectiontitle'},
+            {'table_key': 'journalmanager_issue_section'},
+        ],
+    },
+    {
+        'table_key': 'journalmanager_journal_abstract_keyword_languages',
+        'tables_names': [
+            {'table_key': 'journalmanager_language'}
+        ],
+    },
+    {
+        'table_key': 'journalmanager_journal_subject_categories',
+        'tables_names': [
+            {'table_key': 'journalmanager_subjectcategory'},
+        ],
+    },
+    {
+        'table_key': 'journalmanager_journal_study_areas',
+        'tables_names': [
+            {'table_key': 'journalmanager_studyareas'},
+        ],
+    },
+    {'table_key': 'journalmanager_journal_languages'},
+    {'table_key': 'journalmanager_journaltitle'},
+    {'table_key': 'journalmanager_journalmission'},
+    {'table_key': 'journalmanager_uselicense'},
+]
+
+
+collection_tables = [
+    {
+        'table_key': 'journalmanager_membership',
+        'tables_names': [
+            {
+                'table_key': 'journalmanager_journal',
+                'tables_names': journals_tables,
+            },
+        ],
+    },
+    {
+        'table_key': 'journalmanager_sponsor_collections',
+        'tables_names': [
+            {
+                'table_key': 'journalmanager_sponsor',
+                'tables_names': [
+                    {'table_key': 'journalmanager_institution'},
+                    {'table_key': 'journalmanager_journal_sponsor'},
+                ],
+            },
+        ],
+    },
+    {'table_key': 'journalmanager_journaltimeline'},
+]
+
+
+db_tables = {
+    'table_key': 'journalmanager_collection',
+    'tables_names': collection_tables,
+},
+
+
+class JournalManagerDBError(Exception):
+    """Exceção na execução de SQL query"""
+
+
+class JournalManagerDB:
+    def __init__(self, uri):
+        self._uri = uri
+        self._engine = None
+        self._connection = None
+        self._metadata = None
+
+    @property
+    def engine(self):
+        if not self._engine:
+            self._engine = sqlalchemy.create_engine(self._uri, echo=True)
+        return self._engine
+
+    @property
+    def metadata(self):
+        if not self._metadata:
+            self._metadata = sqlalchemy.MetaData(bind=self.engine)
+        return self._metadata
+
+    @property
+    def conn(self):
+        if not self._connection:
+            self._connection = self.engine.connect()
+        return self._connection
+
+    def execute_sql(self, sql_command):
+        try:
+            result = self.conn.execute(sql_command)
+        except sqlalchemy.exc.ProgrammingError as exc:
+            raise JournalManagerDBError(str(exc)) from None
+        else:
+            return result.fetchall()
+
+    def collect_tables_names(self):
+        result = self.execute_sql(
+            "select table_name from information_schema.tables "
+            "where table_schema='public' and table_name like journalmanager_%"
+        )
+        return [table[0] for table in result]
+
+    def get_table_struct(self, table_name):
+        try:
+            return sqlalchemy.Table(
+                table_name, self.metadata, autoload=True, autoload_with=self.engine)
+        except sqlalchemy.exc.OperationalError as exc:
+            raise JournalManagerDBError(str(exc)) from None
+
+    def get_collection(self, collection_acronym):
+        collection = self.get_table_struct('journalmanager_collection')
+        try:
+            collections = collection.select().where(
+                collection.c.acronym == collection_acronym
+            ).execute().fetchall()
+        except sqlalchemy.exc.OperationalError as exc:
+            raise JournalManagerDBError(str(exc)) from None
+        else:
+            return {
+                column: value
+                for column, value in zip(collection.c.keys(), collections[0])
+            }
+
+    def get_journals_by_collection(self, collection_acronym):
+        # def default(o):
+        #     if isinstance(o, (datetime.date, datetime.datetime)):
+        #         return o.isoformat()
+
+        collection = self.get_table_struct('journalmanager_collection')
+        journal = self.get_table_struct('journalmanager_journal')
+        membership = self.get_table_struct('journalmanager_membership')
+        try:
+            journals = journal.select().select_from(
+                journal.join(membership.join(collection))
+            ).where(
+                collection.c.acronym == collection_acronym
+            ).execute().fetchall()
+        except sqlalchemy.exc.OperationalError as exc:
+            raise JournalManagerDBError(str(exc)) from None
+        else:
+            return [
+                {
+                    column: value
+                    for column, value in zip(journal.c.keys(), line)
+                }
+                for line in journals
+            ]
 
 
 def ext_identifiers():
@@ -25,6 +223,42 @@ def ext_journal(issn):
         params={"collection": config.get("SCIELO_COLLECTION"), "issn": issn},
     ).json()
     return Journal(journal[0])
+
+
+def serialize_data(table_struct, relation_object):
+    if relation_object:
+        queryset = table_struct.select().select_from(
+            table_struct.join(relation_object.join(collection))
+        ).where(
+            collection.c.acronym == collection_acronym
+        ).execute().fetchall()
+        [
+            {
+                column: value
+                for column, value in zip(table_struct.c.keys(), line)
+            }
+            for line in queryset
+        ]
+    else:
+        {
+            column: value
+            for column, value in zip(collection.c.keys(), collections[0])
+        }
+
+
+def read_and_save_tables(get_table_struct, db_tables, relation_object=None):
+    table_struct = get_table_struct(db_tables['table_key'])
+    serialized = serialize_data(table_struct, relation_object)
+    save_data(serialized)
+
+    for table in db_tables['tables_names'] or []:
+        read_and_save_tables(get_table_struct, table, table_struct)
+
+
+def ext_journal_from_manager():
+    journal_manager_db = JournalManagerDB(config.get("SCIELO_MANAGER_DB"))
+    tables = journal_manager_db.collect_tables_names()
+    read_and_save_tables(get_table_struct, db_tables)
 
 
 def get_all_journal():
--- tests/test_export.py
+++ tests/test_export.py
@@ -1,8 +1,12 @@
+import datetime
 import os
 import unittest
 from copy import deepcopy
-from unittest.mock import patch, ANY
+from unittest.mock import patch, call, ANY, MagicMock
+
+import sqlalchemy
 from xylose.scielodocument import Journal, Article
+
 from documentstore_migracao.export import journal, article
 from documentstore_migracao import exceptions, config
 from . import SAMPLES_JOURNAL, SAMPLES_ARTICLE, SAMPLES_PATH, utils
@@ -116,3 +120,356 @@ class TestExportArticle(unittest.TestCase):
         obj = Article(copy_SAMPLES_ARTICLE)
         article.get_not_xml_article(obj)
         mk_ext_article_txt.assert_not_called()
+
+
+class TestJournalManagerDB(unittest.TestCase):
+    def setUp(self):
+        self.db_uri = config.get("SCIELO_MANAGER_DB")
+        self.journal_manager_db = journal.JournalManagerDB(uri=self.db_uri)
+
+    def test_init(self):
+        self.assertEqual(self.journal_manager_db._uri, self.db_uri)
+        self.assertIsNone(self.journal_manager_db._engine)
+        self.assertIsNone(self.journal_manager_db._connection)
+        self.assertIsNone(self.journal_manager_db._metadata)
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_engine_new_engine_if_no_engine(self, mock_create_engine):
+        mock_engine = MagicMock()
+        mock_create_engine.return_value = mock_engine
+        self.assertEqual(
+            self.journal_manager_db.engine, self.journal_manager_db._engine)
+        mock_create_engine.assert_called_once_with(self.db_uri, echo=True)
+        self.assertEqual(self.journal_manager_db._engine, mock_engine)
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_engine_returns_engine_if_engine_already_exists(
+        self,
+        mock_create_engine
+    ):
+        self.journal_manager_db._engine = "Engine"
+        self.assertEqual(
+            self.journal_manager_db.engine, self.journal_manager_db._engine)
+        mock_create_engine.assert_not_called()
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.MetaData")
+    def test_metadata_new_metadata_if_no_metadata(self, MockMetaData):
+        mock_engine = MagicMock()
+        self.journal_manager_db._engine = mock_engine
+        MockMetaData.return_value = "MetaData"
+        self.assertEqual(
+            self.journal_manager_db.metadata, self.journal_manager_db._metadata)
+        MockMetaData.assert_called_once_with(bind=mock_engine)
+        self.assertEqual(self.journal_manager_db._metadata, "MetaData")
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.MetaData")
+    def test_metadata_returns_metadata_if_metadata_already_exists(self, MockMetaData):
+        self.journal_manager_db._metadata = "MetaData"
+        self.assertEqual(
+            self.journal_manager_db.metadata, self.journal_manager_db._metadata)
+        MockMetaData.assert_not_called()
+
+    def test_conn_new_connection_if_no_connection(self):
+        mock_conn = MagicMock()
+        mock_engine = MagicMock()
+        mock_engine.connect.return_value = mock_conn
+        self.journal_manager_db._engine = mock_engine
+        self.assertEqual(
+            self.journal_manager_db.conn, self.journal_manager_db._connection)
+        mock_engine.connect.assert_called_once_with()
+        self.assertEqual(self.journal_manager_db._connection, mock_conn)
+
+    def test_conn_returns_connection_if_connection_already_exists(self):
+        mock_engine = MagicMock()
+        self.journal_manager_db._connection = "B"
+        self.assertEqual(
+            self.journal_manager_db.conn, self.journal_manager_db._connection)
+        mock_engine.connect.assert_not_called()
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_execute_sql(
+        self,
+        mock_create_engine
+    ):
+        sql_command = "select * from table where id=1"
+        mock_execute_result = MagicMock()
+        mock_conn = MagicMock()
+        mock_conn.execute.return_value = mock_execute_result
+        self.journal_manager_db._connection = mock_conn
+        self.journal_manager_db.execute_sql(sql_command)
+        mock_conn.execute.assert_called_once_with(sql_command)
+        mock_execute_result.fetchall.assert_called_once_with()
+    
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_execute_sql_raises_exception_if_execute_error(
+        self,
+        mock_create_engine
+    ):
+        sql_command = "select * from table where id=1"
+        mock_execute_result = MagicMock()
+        mock_conn = MagicMock()
+        mock_conn.execute = MagicMock(
+            side_effect=sqlalchemy.exc.ProgrammingError(sql_command, "", ""),
+            return_value=mock_execute_result
+        )
+        self.journal_manager_db._connection = mock_conn
+        self.assertRaises(
+            journal.JournalManagerDBError,
+            self.journal_manager_db.execute_sql,
+            sql_command,
+        )
+        mock_execute_result.fetchall.assert_not_called()
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_execute_sql_returns_fetch_result(
+        self,
+        mock_create_engine
+    ):
+        sql_command = "select * from table where id=1"
+        sql_result = [
+            (1, "a",),
+            (1, "b",),
+            (1, "c",),
+        ]
+        mock_execute_result = MagicMock()
+        mock_execute_result.fetchall.return_value = sql_result
+        mock_conn = MagicMock()
+        mock_conn.execute.return_value = mock_execute_result
+        self.journal_manager_db._connection = mock_conn
+        result = self.journal_manager_db.execute_sql(sql_command)
+        self.assertEqual(result, sql_result)
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_collect_tables_names_exception(
+        self,
+        mock_create_engine
+    ):
+        with patch.object(self.journal_manager_db, "execute_sql") as mock_execute_sql:
+            mock_execute_sql.side_effect = journal.JournalManagerDBError()
+            self.assertRaises(
+                journal.JournalManagerDBError,
+                self.journal_manager_db.collect_tables_names
+            )
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_collect_tables_names(
+        self,
+        mock_create_engine
+    ):
+        with patch.object(self.journal_manager_db, "execute_sql") as mock_execute_sql:
+            tables = [
+                ('auth_user',),
+                ('django_content_type',),
+                ('django_flatpage',),
+            ]
+            mock_execute_sql.return_value = tables
+            result = self.journal_manager_db.collect_tables_names()
+            mock_execute_sql.assert_called_once_with(
+                "select table_name from information_schema.tables "
+                "where table_schema='public' and table_name like journalmanager_%"
+            )
+            self.assertEqual(result, [table[0] for table in tables])
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.Table")
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_get_table_struct_exception(
+        self,
+        mock_create_engine,
+        MockTable
+    ):
+        MockTable.side_effect = sqlalchemy.exc.OperationalError("table_name", "", "")
+        self.assertRaises(
+            journal.JournalManagerDBError,
+            self.journal_manager_db.get_table_struct,
+            "table_name"
+        )
+
+    @patch("documentstore_migracao.export.journal.sqlalchemy.Table")
+    @patch("documentstore_migracao.export.journal.sqlalchemy.create_engine")
+    def test_get_table_struct(
+        self,
+        mock_create_engine,
+        MockTable
+    ):
+        self.journal_manager_db._engine = "Engine"
+        self.journal_manager_db._metadata = "MetaData"
+        MockTable.return_value = "AuthUserTable"
+        table_struct = self.journal_manager_db.get_table_struct("auth_user")
+        MockTable.assert_called_once_with(
+            "auth_user",
+            "MetaData",
+            autoload=True,
+            autoload_with="Engine"
+        )
+        self.assertEqual(table_struct, "AuthUserTable")
+
+    def test_get_collection_gets_table_struct(self):
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            mock_get_table_struct.return_value = MagicMock()
+            self.journal_manager_db.get_collection('spa')
+            mock_get_table_struct.assert_called_once_with('journalmanager_collection')
+
+    def test_get_collection_exception(self):
+        mock_collection_table = MagicMock()
+        mock_collection_table.select.return_value.where.return_value.\
+            execute.return_value.fetchall.side_effect = sqlalchemy.exc.OperationalError(
+                "table_name", "", "")
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            mock_get_table_struct.return_value = mock_collection_table
+            self.assertRaises(
+                journal.JournalManagerDBError,
+                self.journal_manager_db.get_collection,
+                'spa'
+            )
+
+    def test_get_collection_returns_serialized_journal(self):
+        columns = ['column_1', 'column_2', 'column_3', ]
+        lines = [
+            ('value_1_1', datetime.datetime(2018, 1, 2), 'value_1_3',),
+            ('value_2_1', datetime.datetime(2018, 2, 2), 'value_2_3',),
+            ('value_3_1', datetime.datetime(2018, 3, 2), 'value_3_3',),
+        ]
+        expected = {
+            column: value
+            for column, value in zip(columns, lines[0])
+        }
+        mock_collection_table = MagicMock()
+        mock_collection_table.c.keys.return_value = columns
+        mock_collection_table.select.return_value.where.return_value.\
+            execute.return_value.fetchall.return_value = lines
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            mock_get_table_struct.return_value = mock_collection_table
+            journal_dict = self.journal_manager_db.get_collection('spa')
+            self.assertEqual(journal_dict, expected)
+
+    def test_get_journals_by_collection_gets_tables_structs(self):
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            self.journal_manager_db.get_journals_by_collection('spa')
+            calls = [
+                call('journalmanager_collection'),
+                call('journalmanager_journal'),
+                call('journalmanager_membership')
+            ]
+            mock_get_table_struct.assert_has_calls(calls, any_order=True)
+
+    def test_get_journals_by_collection_exception(self):
+        mock_collection_table = MagicMock()
+        mock_journal_table = MagicMock()
+        mock_journal_table.select.return_value.select_from.return_value.\
+            where.return_value.execute.return_value.fetchall.side_effect = \
+            sqlalchemy.exc.OperationalError("table_name", "", "")
+        mock_membership_table = MagicMock()
+        tables_structs = [
+            mock_collection_table,
+            mock_journal_table,
+            mock_membership_table,
+        ]
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            mock_get_table_struct.side_effect = tables_structs
+            self.assertRaises(
+                journal.JournalManagerDBError,
+                self.journal_manager_db.get_journals_by_collection,
+                'spa'
+            )
+
+    def test_get_journals_by_collection_returns_serialized_journal(self):
+        columns = ['column_1', 'column_2', 'column_3', ]
+        lines = [
+            ('value_1_1', datetime.datetime(2018, 1, 2), 'value_1_3',),
+            ('value_2_1', datetime.datetime(2018, 2, 2), 'value_2_3',),
+            ('value_3_1', datetime.datetime(2018, 3, 2), 'value_3_3',),
+        ]
+        expected = [
+            {
+                column: value
+                for column, value in zip(columns, line)
+            }
+            for line in lines
+        ]
+        mock_collection_table = MagicMock()
+        mock_journal_table = MagicMock()
+        mock_journal_table.c.keys.return_value = columns
+        mock_journal_table.select.return_value.select_from.return_value.\
+            where.return_value.execute.return_value.fetchall.return_value = lines
+        mock_membership_table = MagicMock()
+        tables_structs = [
+            mock_collection_table,
+            mock_journal_table,
+            mock_membership_table,
+        ]
+        with patch.object(self.journal_manager_db, "get_table_struct") \
+                as mock_get_table_struct:
+            mock_get_table_struct.side_effect = tables_structs
+            journal_dict = self.journal_manager_db.get_journals_by_collection('spa')
+            self.assertEqual(journal_dict, expected)
+
+
+class TestExportJournalFromManager(unittest.TestCase):
+    @patch("documentstore_migracao.export.journal.JournalManagerDB")
+    def test_config_database(self, MockJournalManagerDB):
+        journal.ext_journal_from_manager()
+        MockJournalManagerDB.assert_called_once_with(config.get("SCIELO_MANAGER_DB"))
+
+    @patch.object(journal.JournalManagerDB, "get_journals_by_collection")
+    @patch.object(journal.JournalManagerDB, "collect_tables_names")
+    def test_collect_tables_names(
+        self,
+        mock_collect_tables_names,
+        mock_get_journals_by_collection
+    ):
+        journal.ext_journal_from_manager()
+        mock_collect_tables_names.assert_called_once_with()
+
+    @patch.object(journal.JournalManagerDB, "get_journals_by_collection")
+    @patch.object(journal.JournalManagerDB, "collect_tables_names")
+    def test_get_journals_by_collection(
+        self,
+        mock_collect_tables_names,
+        mock_get_journals_by_collection
+    ):
+        journal.ext_journal_from_manager()
+        mock_get_journals_by_collection.assert_called_once_with(
+            config.get("SCIELO_COLLECTION")
+        )
+
+    @patch.object(journal.JournalManagerDB, "get_journals_by_collection")
+    @patch.object(journal.JournalManagerDB, "collect_tables_names")
+    def test_save_journals(
+        self,
+        mock_collect_tables_names,
+        mock_get_journals_by_collection
+    ):
+        journal.ext_journal_from_manager()
+        mock_get_journals_by_collection.return_value = [
+            # {
+            #     "scielo_issn": "print",
+            #     "is_indexed_scie": False,
+            #     "id": 44,
+            #     "eletronic_issn": "1518-8787",
+            #     "publisher_country": "BR",
+            #     "previous_title_id": None,
+            #     "subject_descriptors": "saude coletiva\\r\\nsaude publica\\r\\nmicrobiologia",
+            #     "title_iso": "Rev. sa\\u00fade p\\u00fablica",
+            #     "title": "Revista de Sa\\u00fade P\\u00fablica",
+            #     "short_title": "Rev. Sa\\u00fade P\\u00fablica",
+            #     "secs_code": "",
+            #     "editor_address_state": "SP",
+            #     "print_issn": "0034-8910",
+            #     "copyrighter": "Faculdade de Sa\\u00fade P\\u00fablica da Universidade de S\\u00e3o Paulo",
+            #     "init_vol": "1",
+            #     "publisher_state": "SP",
+            #     "ctrl_vocabulary": "decs",
+            #     "is_indexed_ssci": False,
+            #     "url_online_submission": "http://mc04.manuscriptcentral.com/rsp-scielo",
+            #     "creator_id": 11,
+            #     "acronym": "rsp",
+            #     "created": datetime.datetime(1998, 4, 30, 3),
+            #     "init_year": "1967",
+            #     "url_journal": ""
+            # },
+        ]
